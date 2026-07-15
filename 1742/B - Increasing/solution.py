3820c19f20e51a0867ce81ea849f07a2654dc968
Surbhi Bajj <surbhibajj9562@gmail.com>
2026-07-15 23:47:35 +0530

    Add Increasing [B] from Codeforces

---
+++ 1742/B - Increasing/solution.py
@@ -0,0 +1,10 @@
+t = int(input())
+ 
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    if len(set(arr)) == n:
+        print("YES")
+    else:
+        print("NO")
