7c4ff0a5a00098a6fa2438d83f23933e6e8f9425
Surbhi Bajj <surbhibajj9562@gmail.com>
2026-07-15 23:37:13 +0530

    Add Drinks [B] from Codeforces

---
+++ 200/B - Drinks/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    
+    double sum = 0, x;
+    for(int i = 0; i < n; i++) {
+        cin >> x;
+        sum += x;
+    }
+    
+    cout << fixed << setprecision(6) << sum / n;
+    return 0;
+}
