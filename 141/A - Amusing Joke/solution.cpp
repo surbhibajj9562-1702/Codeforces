2db037f4409d77aa93b5be89a6d26d2402538dbf
Surbhi Bajj <surbhibajj9562@gmail.com>
2026-07-15 23:38:53 +0530

    Add Amusing Joke [A] from Codeforces

---
+++ 141/A - Amusing Joke/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+    string a, b, c;
+    cin >> a >> b >> c;
+ 
+    string ab = a + b;
+ 
+    sort(ab.begin(), ab.end());
+    sort(c.begin(), c.end());
+ 
+    if (ab == c)
+        cout << "YES";
+    else
+        cout << "NO";
+ 
+    return 0;
+}
