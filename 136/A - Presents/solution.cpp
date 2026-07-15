d0f7eeee96bc3d6d9186444670390e591a199367
Surbhi Bajj <surbhibajj9562@gmail.com>
2026-07-15 23:37:17 +0530

    Add Presents [A] from Codeforces

---
+++ 136/A - Presents/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    int p[101], ans[101];
+ 
+    for (int i = 1; i <= n; i++) {
+        cin >> p[i];
+    }
+ 
+    for (int i = 1; i <= n; i++) {
+        ans[p[i]] = i;
+    }
+ 
+    for (int i = 1; i <= n; i++) {
+        cout << ans[i] << " ";
+    }
+ 
+    return 0;
+}
