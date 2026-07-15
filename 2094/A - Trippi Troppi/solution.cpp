bbb3281883739ebcf8e0ae03d171f02085fe4c71
Surbhi Bajj <surbhibajj9562@gmail.com>
2026-07-15 23:41:58 +0530

    Add Trippi Troppi [A] from Codeforces

---
+++ 2094/A - Trippi Troppi/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        string a, b, c;
+        cin >> a >> b >> c;
+        
+        cout << a[0] << b[0] << c[0] << endl;
+    }
+ 
+    return 0;
+}
