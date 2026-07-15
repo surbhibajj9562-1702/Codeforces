6e1bd141792ea17ba9761c2d0577847dd5fa94d9
Surbhi Bajj <surbhibajj9562@gmail.com>
2026-07-15 23:50:22 +0530

    Add Beautiful Average [A] from Codeforces

---
+++ 2162/A - Beautiful Average/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        cout << *max_element(a.begin(), a.end()) << endl;
+    }
+    return 0;
+}
