9270e1e293bae3dcccbe391771c0b9d53788a264
Surbhi Bajj <surbhibajj9562@gmail.com>
2026-07-15 23:40:02 +0530

    Add Odd One Out [A] from Codeforces

---
+++ 1915/A - Odd One Out/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << (a ^ b ^ c) << '
+';
+    }
+    return 0;
+}
