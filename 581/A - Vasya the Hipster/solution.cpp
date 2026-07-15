8906e2732379c1214a973a31cc9ac914991cb37b
Surbhi Bajj <surbhibajj9562@gmail.com>
2026-07-15 23:41:04 +0530

    Add Vasya the Hipster [A] from Codeforces

---
+++ 581/A - Vasya the Hipster/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a, b;
+    cin >> a >> b;
+ 
+    int different = min(a, b);
+    int same = (abs(a - b)) / 2;
+ 
+    cout << different << " " << same << endl;
+ 
+    return 0;
+}
